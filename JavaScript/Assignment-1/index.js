47772c91123de0dcdc47475fafaf11535f7da746
Ahmed Raza <raza403@gmail.com>
2018-01-22 22:27:53 +0500

    Update

--- JavaScript/Assignment-1/index.js
+++ JavaScript/Assignment-1/index.js
@@ -1,6 +1,5 @@
-*Name this external file gallery.js*/
-
-function upDate(previewPic){
+/* Name this external file gallery.js */
+function upDate(previewPic) { 
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
     to the source file of the preview image
